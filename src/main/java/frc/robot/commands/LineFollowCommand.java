9b6ef3a40d8e7443409c269bc4b39d646996982c
Valerie <valukutty.val@gmail.com>
2019-02-09 10:48:22 -0500

    deleted line follow timeout

--- src/main/java/frc/robot/commands/LineFollowCommand.java
+++ src/main/java/frc/robot/commands/LineFollowCommand.java
@@ -13,7 +13,6 @@ import frc.robot.Robot;
 public class LineFollowCommand extends Command {
   public LineFollowCommand() {
     requires(Robot.drivetrainSubsystem);
-    // setTimeout(0.5);
   }
 
   // Called just before this Command runs the first time
